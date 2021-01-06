c30f214c07d749ba024584a985683d54664e5a1e
Alex <alexislafrances-laf@outlook.fr>
2021-01-06 15:43:52 +0100

    SERVER FINISH

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,13 @@
 #    By: alafranc <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/17 14:19:53 by alafranc          #+#    #+#              #
-#    Updated: 2021/01/05 13:52:00 by alafranc         ###   ########lyon.fr    #
+#    Updated: 2021/01/05 16:32:43 by alafranc         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 FROM debian:buster
 
-RUN apt-get update && apt-get install -y openssl && apt-get install -y sudo && apt-get install -y nginx && sudo apt-get install -y mariadb-server && sudo apt-get install -y php-fpm php-mysql
+RUN apt-get update && apt-get install -y openssl && apt-get install -y sudo && apt-get install -y nginx && sudo apt install -y mariadb-server && sudo apt install -y php-fpm php-mysql
 
 COPY srcs/ . 
 
--- srcs/script.sh
+++ srcs/script.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-
 #SERVICE LAUNCH
+service php7.3-fpm start
+service nginx start
+
 #SSL CERTIFICATE
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
-#sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
+sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
 sudo mv self-signed.conf /etc/nginx/snippets/
 sudo mv ssl-params.conf /etc/nginx/snippets/
-sudo mv default /etc/nginx/sites-available/
+sudo cp /nginx_params/default /etc/nginx/sites-available/default
 
 #MYSQL
 service mysql start
 sudo mysql_secure_installation
-#mysql < mysql.sql
-
-#SERVICE LAUNCH
-service nginx restart
-service mysql restart
-service php7.3-fpm restart
-
+mysql -u root  < mysql.sql
 
 #PUT WORDPRESS
-sudo mv wordpress/* /var/www/html/
-rmdir wordpress
+sudo mv wordpress/ /var/www/html/
+
+#PUT PphMyAdmin
+sudo mv phpMyAdmin /var/www/html/
 
 #DISPLAY LOGS
+service nginx restart
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log
